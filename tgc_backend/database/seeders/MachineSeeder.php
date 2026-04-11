1257a55528f4bdb1a92d097196285bc974a06fe3
Muhammadqodir <m.qodir777@gmail.com>
2026-04-11 19:10:08 +0500

    Update MachineSeeder.php

--- tgc_backend/database/seeders/MachineSeeder.php
+++ tgc_backend/database/seeders/MachineSeeder.php
@@ -10,16 +10,16 @@ class MachineSeeder extends Seeder
     public function run(): void
     {
         $machines = [
-            ['name' => 'Mashina 1', 'model_name' => 'Van de Wiele SRX-200'],
-            ['name' => 'Mashina 2', 'model_name' => 'Van de Wiele SRX-200'],
-            ['name' => 'Mashina 3', 'model_name' => 'Van de Wiele SRX-200'],
-            ['name' => 'Mashina 4', 'model_name' => 'Van de Wiele SRX-200'],
-            ['name' => 'Mashina 5', 'model_name' => 'Van de Wiele SRX-200'],
-            ['name' => 'Mashina 6', 'model_name' => 'Van de Wiele SRX-200'],
-            ['name' => 'Mashina 7', 'model_name' => 'Van de Wiele SRX-200'],
-            ['name' => 'Mashina 8', 'model_name' => 'Van de Wiele SRX-200'],
-            ['name' => 'Mashina 9', 'model_name' => 'Van de Wiele SRX-200'],
-            ['name' => 'Mashina 10', 'model_name' => 'Van de Wiele SRX-200'],
+            ['name' => 'Stanok 1', 'model_name' => 'Van de Wiele'],
+            ['name' => 'Stanok 2', 'model_name' => 'Van de Wiele'],
+            ['name' => 'Stanok 3', 'model_name' => 'Van de Wiele'],
+            ['name' => 'Stanok 4', 'model_name' => 'Van de Wiele'],
+            ['name' => 'Stanok 5', 'model_name' => 'Van de Wiele'],
+            ['name' => 'Stanok 6', 'model_name' => 'Van de Wiele'],
+            ['name' => 'Stanok 7', 'model_name' => 'Van de Wiele'],
+            ['name' => 'Stanok 8', 'model_name' => 'Van de Wiele'],
+            ['name' => 'Stanok 9', 'model_name' => 'Van de Wiele'],
+            ['name' => 'Stanok 10', 'model_name' => 'Van de Wiele'],
         ];
 
         foreach ($machines as $machine) {
